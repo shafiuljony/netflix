d24470dc7c80a2d8b4f4350835f741fc7cd090e6
shafiuljony <shafiuljony12@gmail.com>
2022-05-18 21:44:45 +0600

    jumbotron

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import jumboDate from '../src/fixtures/jumbo.json';
+import Jumbotron from './components/jumbotron';
 export default function App() {
   return (
-    <p>Hello Netflix</p>
+    <Jumbotron.Container>
+      {jumboDate.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction} >
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.image} />
+          </Jumbotron.Pane>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
   );
 }
