7b01133abd9a4bea5bd41a97c5841d676142a058
shafiuljony <shafiuljony12@gmail.com>
2022-05-19 14:33:42 +0600

    export jumbotron

--- src/app.js
+++ src/app.js
@@ -1,21 +1,6 @@
 import React from 'react';
-import jumboDate from './fixtures/jumbo.json';
-import Jumbotron from './components/jumbotron';
+import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
-  return (
-    <Jumbotron.Container>
-      {jumboDate.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <Jumbotron.Pane>
-            <Jumbotron.Title>{item.title}</Jumbotron.Title>
-            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
-          </Jumbotron.Pane>
-          <Jumbotron.Pane>
-            <Jumbotron.Image src={item.image} alt={item.image} />
-          </Jumbotron.Pane>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
+  return <JumbotronContainer />;
 }
