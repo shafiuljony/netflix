acbc29b944c30d5594fef7c00d210ecae1200d36
shafiuljony <shafiuljony12@gmail.com>
2022-05-20 12:30:55 +0600

    useContext and compound Component

---
+++ src/components/accordion/styles/index.js
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useContext, Children } from 'react';
+
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useState(false);
+
+  return (
+    <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>
+    </ToggleContext.Provider>
+  );
+};
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  const [toggleShow, setToggleShow] = useContext(ToggleContext);
+
+  return (
+    <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+      {children}
+    </Header>
+  );
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext);
+
+  return toggleShow ? <Body {...restProps}>{Children}</Body> : null;
+};
