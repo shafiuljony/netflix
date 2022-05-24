7a2d436237583517a5065ea298bfc9e1dd37604d
shafiuljony <shafiuljony12@gmail.com>
2022-05-25 00:33:13 +0600

    Opt-form

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import faqsData from '../fixtures/faqs.json';
 import { Accordion } from '../components';
+import OptForm from '../components/opt-form';
 
 export function FaqsContainer() {
   return (
@@ -10,9 +11,17 @@ export function FaqsContainer() {
         <Accordion.Item key={item.id}>
           <Accordion.Header>{item.header}</Accordion.Header>
           <Accordion.Body>{item.body}</Accordion.Body>
-          <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+
+      <Accordion.Item>
+        <OptForm>
+          <OptForm.Input placeholder="Email address" />
+          <OptForm.Button>Try it now</OptForm.Button>
+          <OptForm.Break />
+          <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+        </OptForm>
+      </Accordion.Item>
     </Accordion>
   );
 }
