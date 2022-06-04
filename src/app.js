4ad0fbed714d20a5f62c854fb90f2b5ab7ae0338
shafiuljony <shafiuljony12@gmail.com>
2022-06-04 17:27:22 +0600

    Routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { JumbotronContainer } from './containers/jumbotron';
-import { FooterContainer } from './containers/footer';
-import { FaqsContainer } from './containers/faqs';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+import { Home, Browse, Signin, Signup } from './pages';
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Route exect path="/browse">
+        <Browse />
+      </Route>
+      <Route exect path="/signin">
+        <Signin />
+      </Route>
+      <Route exect path="/signup">
+        <Signup />
+      </Route>
+      <Route exect path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Router>
   );
 }
---
+++ src/constants/routes.js
@@ -0,0 +1,4 @@
+export const HOME = '/';
+export const BROWSE = '/browse';
+export const SING_UP = '/sing-up';
+export const SING_IN = '/sing-in';
