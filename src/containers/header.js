a6f650f37a7a2c2f2a005614f0fec329f797ad83
shafiuljony <shafiuljony12@gmail.com>
2022-06-05 19:17:38 +0600

    sing_in page

--- src/containers/header.js
+++ src/containers/header.js
@@ -1,14 +1,15 @@
-import React, { Children } from 'react';
+import React from 'react';
 import Header from '../components/header';
+import * as ROUTES from '../constants/routes';
 
 export function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo />
-        <Header.Button />
+        <Header.Logo to={ROUTES.HOME} alt="netflix" />
+        <Header.ButtonLink to={ROUTES.SING_IN}>Sign In</Header.ButtonLink>
       </Header.Frame>
-      {Children}
+      {children}
     </Header>
   );
 }
