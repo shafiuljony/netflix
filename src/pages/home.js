e392d8c905f68afe293cb84ce3e2c20c64907ca3
shafiuljony <shafiuljony12@gmail.com>
2022-06-05 23:56:58 +0600

    heder completed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { OptForm } from '../components';
+import { Feature, OptForm } from '../components';
 import { HeaderContainer } from '../containers/header';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
@@ -9,12 +9,15 @@ export default function Home() {
   return (
     <>
       <HeaderContainer>
-        <OptForm>
-          <OptForm.Input placeholder="Email address" />
-          <OptForm.Button>Try it now</OptForm.Button>
-          <OptForm.Break />
-          <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
-        </OptForm>
+        <Feature>
+          <Feature.Title>Unlimited films, Tv programmes and more.</Feature.Title>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+          </OptForm>
+        </Feature>
       </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
