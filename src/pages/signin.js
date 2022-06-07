a123a4d7711898193438a8149fe41f80db46e78f
shafiuljony <shafiuljony12@gmail.com>
2022-06-07 23:53:31 +0600

    form

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import { Form } from '../components';
 
 export default function Signin() {
-  return <p>Hello from the sign in page!</p>;
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState('');
+
+  // check from input element as valid
+  // email & password
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Frame.Title>Sign In</Frame.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit />
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
