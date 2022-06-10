9b595010b6bb938bc675d5cb4a9fa0f8e66e2ce2
shafiuljony <shafiuljony12@gmail.com>
2022-06-10 11:59:30 +0600

    form complete

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -42,6 +42,12 @@ export default function Signin() {
               Sign In
             </Form.Submit>
           </Form.Base>
+          <Form.Text>
+            New to Netflix> <Form.Link to="/signup">Sign Up</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot/ Learn more.
+          </Form.TextSmall>
         </Form>
       </HeaderContainer>
       <FooterContainer />
